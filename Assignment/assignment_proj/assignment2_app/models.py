68e6f1215a06b94d0816f4e699c2cd3bf4cbd900
AimanLubis <s362319@students.cdu.edu.au>
2024-05-20 09:38:34 +0930

    Push

--- Assignment/assignment_proj/assignment2_app/models.py
+++ Assignment/assignment_proj/assignment2_app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-  TID = models.CharField(max_length=3, primary_key=True)
+  tid = models.CharField(max_length=3, primary_key=True)
   title = models.CharField(max_length=100)
   desc = models.CharField(max_length=1500)
   supName = models.CharField(max_length=30)
   
   def __str__(self):
-    return self.TID + " : " + self.title
+    return self.tid + " : " + self.title
